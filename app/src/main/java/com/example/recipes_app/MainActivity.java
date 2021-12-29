f3c85cc2b4857d8cbb3531fe443654d5778e0324
barelimelech <barelimelech01@gmail.com>
2021-12-29 13:07:17 +0200

    Navigation

--- app/src/main/java/com/example/recipes_app/MainActivity.java
+++ app/src/main/java/com/example/recipes_app/MainActivity.java
@@ -1,16 +1,11 @@
 package com.example.recipes_app;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
 import android.view.Menu;
 
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
-import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
-import androidx.navigation.NavHost;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
@@ -30,32 +25,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
-//        NavHost navHost = (NavHost)getSupportFragmentManager().findFragmentById(R.id.base_navhost);
-//        navCtl = navHost.getNavController();
-//
-//        NavigationUI.setupActionBarWithNavController(this,navCtl);
-
-
-
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
-//        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.myAccountFragment, R.id.newRecipeFragment, R.id.searchFragment)
+                R.id.nav_home, R.id.myAccount_nav, R.id.newRecipeFragment, R.id.searchFragment)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
--- app/src/main/java/com/example/recipes_app/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/recipes_app/ui/search/SearchFragment.java
@@ -1,4 +1,4 @@
-package com.example.recipes_app;
+package com.example.recipes_app.ui.search;
 
 import android.os.Bundle;
 
@@ -8,12 +8,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class SubCategoryFragment extends Fragment {
+import com.example.recipes_app.R;
+
+public class SearchFragment extends Fragment {
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sub_category, container, false);
+        return inflater.inflate(R.layout.fragment_search, container, false);
     }
 }
